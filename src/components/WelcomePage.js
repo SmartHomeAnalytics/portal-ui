51b28da482644d679c75cfa1baa717159e9fccc3
Vlad Shchehlov <vladyslav.shchehlov@sigma.software>
2022-04-30 13:41:56 +0300

    Update welcome page styling

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -4,31 +4,43 @@ import { makeStyles } from '@mui/styles'
 
 import LoginButton from './login/LoginButton'
 import LogoutButton from './login/LogoutButton'
+import SmartHomeLogo from './icons/SmartHomeLogo'
+import WelcomeImage from './icons/WelcomeImage'
 
 const useStyles = makeStyles(theme => ({
   root: {
     height: '100%',
-    backgroundColor: theme.colors.primarySolidBackground,
   },
   aboutUs: {
     marginTop: theme.spacing(16),
-    paddingRight: theme.spacing(4),
+    padding: theme.spacing(8),
+    borderRadius: '25px 0 0 0',
+    backgroundColor: theme.colors.darkBackground,
+    boxShadow: theme.constants.welcomeCardShadow,
   },
   aboutUsLabel: {
     textTransform: 'uppercase',
     fontWeight: 200,
-    fontSize: '2.0rem',
+    fontSize: '1.8rem',
+    color: theme.colors.white,
   },
   aboutUsTitle: {
     fontSize: '4.0rem',
     fontWeight: 200,
     lineHeight: 1,
+    color: theme.colors.white,
   },
   aboutUsText: {
     marginTop: theme.spacing(3),
+    color: theme.colors.white,
   },
-  loginButton: {
-    marginTop: theme.spacing(3),
+  actionButton: {
+    marginTop: theme.spacing(6),
+  },
+  welcomeImage: {
+    maxHeight: '100%',
+    maxWidth: '100%',
+    objectFit: 'cover',
   },
 }), { name: 'ChatTable' })
 
@@ -37,20 +49,28 @@ const WelcomePage = () => {
 
   return (
     <Grid container className={classes.root}>
-      <Grid container item xs={6} />
-      <Grid container item className={classes.aboutUs} direction="column" xs={6}>
-        <Typography className={classes.aboutUsLabel} variant="h1">About us</Typography>
-        <Typography className={classes.aboutUsTitle}>Welcome to Smart Home Analytics</Typography>
-        <Typography className={classes.aboutUsText} variant="body2">Smart home analytics is a smart service that
-          allows you to configure and manage smart
-          home sensors through the control panel. This dashboard includes some charts, graphs, gauges,
-          and other monitoring components. If some indicators should not exceed a certain threshold,
-          you can set up appropriate alerts - in this case, we will notify you by email or telegram.<br /> <br />
+      <Grid container item xs={6}>
+        <WelcomeImage className={classes.welcomeImage} />
+      </Grid>
+      <Grid container item className={classes.aboutUs} direction="column" justifyContent="space-between" xs={6}>
+        <Grid container item direction="column">
+          <Typography className={classes.aboutUsLabel} variant="h1">About us</Typography>
+          <Typography className={classes.aboutUsTitle}>Welcome to Smart Home Analytics</Typography>
+          <Typography className={classes.aboutUsText} variant="body2">Smart home analytics is a smart service that
+            allows you to configure and manage smart
+            home sensors through the control panel. This dashboard includes some charts, graphs, gauges,
+            and other monitoring components. If some indicators should not exceed a certain threshold,
+            you can set up appropriate alerts - in this case, we will notify you by email or telegram.<br /> <br />
 
-          Don&apos;t lose you chance to make your home even smarter - sign up now!
-        </Typography>
-        <LoginButton className={classes.loginButton} variant="outlined" />
-        <LogoutButton className={classes.loginButton} variant="outlined" />
+            Don&apos;t lose you chance to make your home even smarter - sign up now!
+          </Typography>
+          <LoginButton className={classes.actionButton} color="neonLight" variant="outlined" />
+          <LogoutButton className={classes.actionButton} color="neonLight" variant="outlined" />
+        </Grid>
+
+        <Grid container item justifyContent="center">
+          <SmartHomeLogo fill="#ffffff" height={40} width={40} />
+        </Grid>
       </Grid>
     </Grid>
   )
--- src/muiTheme.js
+++ src/muiTheme.js
@@ -5,11 +5,14 @@ const SECONDARY_TEXT_COLOR = '#383434'
 
 // const ACCENT_COLOR = '#9C27B0'
 const PRIMARY_COLOR = '#0d47a1'
-const SECONDARY_COLOR = '#BADCFF'
+const PRIMARY_LIGHT_COLOR = '#BADCFF'
+const SECONDARY_COLOR = '#03fcf0'
+// const WHITE_COLOR = '#FFFFFF'
 
 const theme = createTheme({
   colors: {
-    primarySolidBackground: SECONDARY_COLOR,
+    primarySolidBackground: PRIMARY_LIGHT_COLOR,
+    darkBackground: PRIMARY_COLOR,
     white: '#ffffff',
   },
   palette: {
@@ -18,8 +21,14 @@ const theme = createTheme({
       main: PRIMARY_COLOR,
     },
     secondary: {
+      main: PRIMARY_LIGHT_COLOR,
+    },
+    neonLight: {
       main: SECONDARY_COLOR,
     },
+  },
+  constants: {
+    welcomeCardShadow: '0 28px 10px 10px #999999',
   },
   typography: {
     fontFamily: [
